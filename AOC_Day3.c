4add4ba7a938a5de60b12c1f225a674b1c982027
Daniella Toth <daniellatoth@me.com>
2024-12-03 16:18:23 -0500

    Adding day 3 files

---
+++ AOC_Day3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+    This is my code for the third day of the Advent of Code 2024
+
+    ** PUZZLE ONE **
+    
+
+    ** PUZZLE TWO **
+    
+
+*/
+
+int main(){
+
+    return 0;
+}
